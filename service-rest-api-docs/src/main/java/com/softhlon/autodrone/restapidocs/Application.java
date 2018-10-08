c05ec1b5fa656ae7c869418448fac5146e91eb0d
Piotr Marat <pmarat@softhlon.com>
2018-10-08 04:12:19 +0200

    first commit

---
+++ service-rest-api-docs/src/main/java/com/softhlon/autodrone/restapidocs/Application.java
@@ -0,0 +1,14 @@
+package com.softhlon.autodrone.restapidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Piotr Marat.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
